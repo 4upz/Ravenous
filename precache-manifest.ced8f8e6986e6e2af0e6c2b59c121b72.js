85b8ee3713712002ff03130d3672cd5b41971466
Arik Stewart <ams382@case.edu>
2020-04-17 02:04:46 -0400

    Updates

---
+++ precache-manifest.ced8f8e6986e6e2af0e6c2b59c121b72.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ced439abbdabd08a23ab90f571653deb",
+    "url": "/Ravenous/index.html"
+  },
+  {
+    "revision": "6ce3da714f400bb3f852",
+    "url": "/Ravenous/static/css/main.2ffe3351.chunk.css"
+  },
+  {
+    "revision": "f964c4e3ab7a514ae181",
+    "url": "/Ravenous/static/js/2.2d239d7d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Ravenous/static/js/2.2d239d7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ce3da714f400bb3f852",
+    "url": "/Ravenous/static/js/main.9f1ca44f.chunk.js"
+  },
+  {
+    "revision": "aebf9374ca003c2325b0",
+    "url": "/Ravenous/static/js/runtime-main.1af2b35f.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/Ravenous/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/Ravenous/static/media/background_search_mobile.5d0905e6.jpg"
+  }
+]);
